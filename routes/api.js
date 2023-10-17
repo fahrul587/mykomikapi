25dc77973f3347da80d383c284eea87fcf9d8cb4
Fahrur Rozi <fahrurrozai587@gmail.com>
2023-10-17 23:35:05 +0700

    route

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,9 @@ const express = require("express")
 const router = express.Router()
 const { recomendation, allSeries, populer, search, details, bacaKomik } = require("../controller")
 
+router.get("/api", (req, res) => {
+    res.json({message: "ok"})
+})
 router.get("/api/recomendation", recomendation)
 router.get("/api/all/:page?", allSeries)
 router.get("/api/populer", populer)
