bb95052762f2f5344ef9b7273b088b66e7e76d0a
Fahrur Rozi <fahrurrozai587@gmail.com>
2024-02-08 23:09:40 +0700

    back

--- index.js
+++ index.js
@@ -2,40 +2,14 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const cors = require("cors")
-const qrcode = require('qrcode-terminal');
 const { default: axios } = require("axios");
 
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const client = new Client({
-    authStrategy: new LocalAuth()
-});
 
 app.use(cors())
 app.use(require("./routes/api"))
 
 app.get("/", (req, res) => {
-    client.on('qr', (qr) => {
-        qrcode.generate(qr, { small: true });
-    });
-    
-    client.on('ready', () => {
-        console.log('Client is ready!');
-    });
-    
-    client.on('message', async (message) => {
-        if (message.body === '!ping') {
-            await message.reply('hell yeah');
-        } else if (message.hasMedia && message.type === "image" || message.type === "document" && message.body.startsWith("#sticker")) {
-            const media = await message.downloadMedia()
-            if (!media.mimetype.includes("image")) {
-                message.reply("Mohon kirim gambar atau GIF untuk dibuat stiker.")
-                return
-            }
-            message.reply(media, undefined, {sendMediaAsSticker: true})
-        }
-    });
-    
-    client.initialize();
+   
     res.json({
         status: 200,
         message:
